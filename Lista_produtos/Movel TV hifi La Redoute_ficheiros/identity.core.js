e247a51d74c7c98d17fc2a73d4d5fdcdb24e105e
Rui <2212550@my.ipleiria.pt>
2022-11-20 15:03:30 +0000

    Listagem de produtos (falta fazer limpeza de codigo)

---
+++ Lista_produtos/Movel TV hifi La Redoute_ficheiros/identity.core.js
@@ -0,0 +1,14 @@
+
+window['ReachFiveConfig'] = {"domain":"la-redoute.reach5.net","countryCode":"FR","clientId":"uVYa4ikI5AbQAxdyrMNB","socialProviders":["google","paypal","facebook","apple","vkontakte"],"customFields":[],"language":"fr","sso":false,"sms":false,"pkce":false,"resourceBaseUrl":"https://assets.reach5.co/hassets/sdk","integrations":{"facebook_ads":{"enabled":false},"ab_tasty":{"enabled":false}},"passwordPolicy":{"minLength":8,"minStrength":2},"scope":"address email events full_write offline_access openid phone profile"};
+
+(function(d,w) {
+    var s = d.createElement('script');
+    s.type = 'text/javascript';
+    s.crossOrigin = true;
+    s.async = true;
+    var allScripts = d.getElementsByTagName('script');
+    var lastParsedScript = allScripts[allScripts.length - 1];
+    s.src = 'https://assets.reach5.co/hassets/sdk/identity.core.07e0f324b9e45bf1d1f1.js';
+    lastParsedScript.parentNode.appendChild(s);
+})(document,window);
+     
